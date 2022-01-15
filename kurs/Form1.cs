32826f09865b30909f45a3da156dbb33b7fcef1c
vxcxvx dgsdg <tima.prelovskiy@mail.ru>
2022-01-15 21:27:14 +0900

    Создал класс частицы и список

--- kurs/Form1.cs
+++ kurs/Form1.cs
@@ -9,12 +9,28 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace kurs
-{
+{ 
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>();
+
         public Form1()
         {
             InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            // генерирую 500 частиц
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                // переношу частицы в центр изображения
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                // добавляю список
+                particles.Add(particle);
+            }
         }
     }
-}
+    }
+
