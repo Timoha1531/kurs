ed3dda4850c40db1a323de705826f212b77a72e8
vxcxvx dgsdg <tima.prelovskiy@mail.ru>
2022-01-16 19:28:14 +0900

    Создал чтобы частицы создавались на курсоре мышки,ну и поменял хар-ки по методичке

--- kurs/Form1.cs
+++ kurs/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace kurs
-{ 
+{
     public partial class Form1 : Form
     {
         List<Particle> particles = new List<Particle>();
@@ -18,19 +18,83 @@ namespace kurs
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
-            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+            
 
-            // генерирую 500 частиц
-            for (var i = 0; i < 500; ++i)
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState(); // каждый тик обновляем систему
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                var particle = new Particle();
-                // переношу частицы в центр изображения
-                particle.X = picDisplay.Image.Width / 2;
-                particle.Y = picDisplay.Image.Height / 2;
-                // добавляю список
-                particles.Add(particle);
+                g.Clear(Color.White);
+                Render(g); // рендерим систему
+            }
+
+            picDisplay.Invalidate();
+        
+    }
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1; // уменьшаю здоровье
+                                    // если здоровье кончилось
+                if (particle.Life < 0)
+                {
+                    particle.Life = 20 + Particle.rand.Next(100); // это не трогаем
+                                                                  // новое начальное расположение частицы — это то, куда указывает курсор
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    // делаю рандомное направление, скорость и размер
+                    particle.Direction = Particle.rand.Next(360);
+                    particle.Speed = 1 + Particle.rand.Next(10);
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    // а это наш старый код
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+            // добавил генерацию частиц
+            // генерирую не более 10 штук за тик
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
+                {
+                    var particle = new Particle();
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
             }
         }
+
+        // функция рендеринга
+        private void Render(Graphics g)
+        {
+            // утащили сюда отрисовку частиц
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            // в обработчике заносим положение мыши в переменные для хранения положения мыши
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
+        }
     }
-    }
+}
 
--- kurs/Particle.cs
+++ kurs/Particle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace kurs
 {
@@ -14,7 +15,7 @@ namespace kurs
 
         public float Direction; // направление движения
         public float Speed; // скорость перемещения
-
+        public float Life;
         // добавили генератор случайных чисел
         public static Random rand = new Random();
 
@@ -25,6 +26,26 @@ namespace kurs
             Direction = rand.Next(360);
             Speed = 1 + rand.Next(10);
             Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100); // Добавили исходный запас здоровья от 20 до 120
+        }
+        public void Draw(Graphics g)
+        {
+
+
+            // рассчитываем коэффициент прозрачности по шкале от 0 до 1.0
+            float k = Math.Min(1f, Life / 100);
+            // рассчитываем значение альфа канала в шкале от 0 до 255
+            // по аналогии с RGB, он используется для задания прозрачности
+            int alpha = (int)(k * 255);
+
+            // создаем цвет из уже существующего, но привязываем к нему еще и значение альфа канала
+            var color = Color.FromArgb(alpha, Color.Black);
+            var b = new SolidBrush(color);
+
+            // остальное все так же
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            b.Dispose();
         }
     }
 }
